a86f42593679886fde772f7d5edff3197c4f1996
truptigaonkar <trups11.trupti@gmail.com>
2019-01-08 11:05:13 +0100

    added .gitignore file to ignore node_modules

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
-const clc = require('cli-color');
-for(let i=0; i<=100; i+=1){
-    if(i%2 ===0){
-        console.log(clc.blue(i));
-    }else{
-        console.log(clc.red(i));
-    }
+
+// // Övning - dependencies
+// const clc = require("cli-color");
+// for (let i = 0; i <= 100; i += 1) {
+//   if (i % 2 === 0) {
+//     console.log(clc.blue(i));
+//   } else {
+//     console.log(clc.red(i));
+//   }
+// }
+
+//Övning - Publicera ett paket till npm
+const clc = require("cli-color");
+if (process.argv[2] === undefined) {
+  console.log("No Argument");
+} else {
+console.log(clc.bold.red(process.argv[2])); 
 }
